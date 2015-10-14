fe6c40ebaedc4f2c540afbe4d230bb2362b03c7e
Tyler jarrard <tjar2074@740s-pl-104.local>
2015-10-14 08:36:44 -0600

    built

---
+++ app/src/main/java/activity/ctec/stateapplication/controller/FrontActivity.java
@@ -0,0 +1,19 @@
+package activity.ctec.stateapplication.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import activity.ctec.stateapplication.R;
+
+public class FrontActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_state);
+    }
+}
---
+++ app/src/main/java/activity/ctec/stateapplication/model/AndroidSaveState.java
@@ -0,0 +1,51 @@
+package activity.ctec.stateapplication.model;
+
+import android.app.Application;
+/**
+ * Created by tjar2074 on 10/14/15.
+ */
+public class AndroidSaveState extends Application
+{
+    private String userName;
+    private boolean isTired;
+    private int age;
+
+    public AndroidSaveState()
+    {
+        this.userName = "default";
+        this.isTired = false;
+        this.age = -0;
+    }
+
+    public String getUserName()
+    {
+        return userName;
+    }
+
+    public boolean getIsTired()
+    {
+        return isTired;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public void setUserName(String userName)
+    {
+        this.userName = userName;
+    }
+
+    public void setIsTired(Boolean isTired)
+    {
+        this.isTired = isTired;
+    }
+
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+
+    
+}
